2071ec8cf393d35acb3c0a073ff8346922091ca9
simph <simph@DESKTOP-EC8H3FT>
2022-01-24 12:05:59 +0200

    Add project files.

---
+++ BabyStore/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BabyStore.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "The category name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessageResourceName = "please enter between 3 and 50 characters in length")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Name must beginning with a capital letter and made up of letters and spaces only")] 
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        public virtual ICollection<Product>Products { get; set; }
+    }
+}
